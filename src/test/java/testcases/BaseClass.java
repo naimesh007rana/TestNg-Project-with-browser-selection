62e89155db10d08a9879ad00f02f30c5bbda3f43
naimesh007rana <naimesh007rana@gmail.com>
2024-03-10 15:51:59 +0000

    Project with browser selection

---
+++ src/test/java/testcases/BaseClass.java
@@ -0,0 +1,60 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+WebDriver driver;
+	
+	private ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
+
+	@BeforeMethod
+	public void SetUp() {
+
+		String DriverType = System.getProperty("Browser");
+		if (DriverType.contains("firefox")) {
+
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+
+		} else {
+
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		
+		SetDriver(driver);
+		GetDriver().get("https://simplilearn.com/");
+		GetDriver().manage().window().maximize();
+		GetDriver().manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+	}
+
+	@AfterMethod
+	public void TearDown() {
+
+		GetDriver().close();
+
+	}
+	
+	public void SetDriver(WebDriver driver) {
+
+		threadLocalDriver.set(driver);
+
+	}
+	
+	public WebDriver GetDriver() {
+
+		return threadLocalDriver.get();
+
+	}
+
+
+}
